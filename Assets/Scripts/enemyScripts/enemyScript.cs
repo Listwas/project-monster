6660b6d7e7edce5badd2a463656a1cbf1383269a
ben <listwa@patrykkr@gmail.com>
2024-10-02 11:34:29 +0200

    enemy shoots, bullets die when touches player

--- Assets/Scripts/enemyScripts/enemyScript.cs
+++ Assets/Scripts/enemyScripts/enemyScript.cs
@@ -4,11 +4,20 @@ using UnityEngine;
 
 public class enemyScript : MonoBehaviour
 {
+    [SerializeField] private float timer = 5;
+    private float bulletTime;
+    public GameObject enemyBullet;
+    public Transform spawnPoint;
+    public float enemySpeed;
     public int maxHealth = 50;
     private int currentHealth;
 
     void Start() {
         currentHealth = maxHealth;
+    }
+
+    void Update() {
+        ShootAtPlayer();
     }
 
     public void TakeDamage(int damage) {
@@ -24,5 +33,18 @@ public class enemyScript : MonoBehaviour
         Debug.Log("Enemy died!");
         // Add death animation or effects here
         Destroy(gameObject);
+    }
+
+    void ShootAtPlayer() {
+        bulletTime -= Time.deltaTime;
+
+        if (bulletTime > 0) return;
+
+        bulletTime = timer;
+        
+        GameObject bulletObj = Instantiate(enemyBullet, spawnPoint.transform.position, spawnPoint.transform.rotation) as GameObject;
+        Rigidbody bulletRig = bulletObj.GetComponent<Rigidbody>();
+        bulletRig.AddForce(bulletRig.transform.forward * enemySpeed);
+        Destroy(bulletObj, 5f);
     }
 }
